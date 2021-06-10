eaa46110d78e66c4d3442c7199c8c6eda1618a2a
Idsanchez0 <davidnofansub@hotmail.es>
2021-06-10 09:48:52 -0500

    Primera Version
    Creacion de Proyecto

---
+++ IsaacPrueba/Data/Repositorio.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsaacPrueba.Data
+{
+    public class Repositorio
+    {
+        private readonly string _connectionString;
+        public Repositorio(IConfiguration configuration) {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+    }
+}
